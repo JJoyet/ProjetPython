85e79252bed752c6491345a958dc1e3d3582e28e
FlorentDB <63601628+FlorentDB@users.noreply.github.com>
2022-12-23 19:00:06 +0100

    rajout qques fcts  pour les LOULOUS

--- classes/PlayerView.py
+++ classes/PlayerView.py
@@ -64,8 +64,8 @@ class PlayerView:
     def put_card(self, i, j):
         if (self.field[i][j] is None):
             return
-        screen.blit(pygame.image.load(self.field[i][j]), (self.i_offset + self.i_factor * i, self.j_offset + self.j_factor * j))
-        pygame.display.flip()
+        screen.blit(pygame.image.load(self.field[i][j].path), (self.i_offset + self.i_factor * i, self.j_offset + self.j_factor * j))
+        pygame.display.flip() #self.field[i][j].path => path = nom de la carte, ex : images/object_cards ,images/PathCards...
 	
     def player_hand(self, player):
         coords = [(687, 53), (781, 53), (875, 53), (687, 109), (781, 109), (875, 109)]
@@ -76,9 +76,128 @@ class PlayerView:
         
         pygame.display.flip()
 
+    def display_name(self):
+        name = pygame.font.Font(None, 32).render("florent", True, pygame.Color("gold")) #affiche le texte scrheck est juste un teste, il faudra utiliser une méthode de la classe player
+        screen.blit(name, (684, 20))
+        pygame.display.flip()
+    
+    def pick_card(self, player): #on clique sur une carte = carte jouée
+        # Create a button that does something when clicked
+        card1 = pygame.Rect(687, 53, 75, 49)
+        card2 = pygame.Rect(781, 53, 75, 49)
+        card3 = pygame.Rect(875, 53, 75, 49)
+        card4 = pygame.Rect(687, 109, 75, 49)
+        card5 = pygame.Rect(781, 109, 75, 49)
+        card6 = pygame.Rect(875, 109, 75, 49)
+        active_card = None
+        active = False
+        done = False
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # If the user clicked on the input_box rect.
+                    if card1.collidepoint(event.pos):
+                        # Toggle the active variable.
+                        active = not active
+                        # Do something
+                        active_card = player.hand[0]
+                        done = True 
+                    elif card2.collidepoint(event.pos):
+                        # Toggle the active variable.
+                        active = not active
+                        # Do something
+                        active_card = player.hand[1]
+                        done = True
+                    elif card3.collidepoint(event.pos):
+                        # Toggle the active variable.
+                        active = not active
+                        # Do something
+                        active_card = player.hand[2]
+                        done = True
+                    elif card4.collidepoint(event.pos):
+                        # Toggle the active variable.
+                        active = not active
+                        # Do something
+                        active_card = player.hand[3]
+                        done = True
+                    elif card5.collidepoint(event.pos):
+                        # Toggle the active variable.
+                        active = not active
+                        # Do something
+                        active_card = player.hand[4]
+                        done = True
+                    elif card6.collidepoint(event.pos):
+                        # Toggle the active variable.
+                        active = not active
+                        # Do something
+                        active_card = player.hand[5]
+                        done = True
+                    else:
+                        active = False
+        return active_card
+        
+    def discard_button(self):
+        discard_but = pygame.Rect(768, 174, 75, 49)
+        color_inactive = pygame.Color('lightskyblue3')
+        color_active = pygame.Color('dodgerblue2')
+        color = color_inactive
+        active = False
+        done = False
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # If the user clicked on the input_box rect.
+                    if discard_but.collidepoint(event.pos):
+                        # Toggle the active variable.
+                        active = not active
+                        # Do something
+                        print("tu as défaussé") #à changer en une méthode qui pioche
+                        done = True 
+                    else:
+                        active = False
+                    # Change the current color of the input box.
+                    color = color_active if active else color_inactive
 
-		
-pygame.display.flip()
+            # Add a white backgournd to the text box
+            pygame.draw.rect(screen, (255, 255, 255), (discard_but.x, discard_but.y, discard_but.w, discard_but.h))
+            # Blit the input_box rect.
+            pygame.draw.rect(screen, color, discard_but, 2)
+            pygame.display.flip()
+            
+    def play_card(self, card, player):
+        discard_but = pygame.Rect(768, 174, 75, 49)
+        done = False
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # If the user clicked on the input_box rect.
+                    x_coord = event.pos[0]//75
+                    y_coord = event.pos[1]//49
+                    if x_coord < 9 :
+                        if self.field[x_coord][y_coord] == None:
+                            self.field[x_coord][y_coord] = card
+                            self.put_card(x_coord,y_coord)
+                            player.hand.remove(card)
+                            done = True
+
+                    if discard_but.collidepoint(event.pos):
+                        # Toggle the active variable.
+                        active = not active
+                        # Do something
+                        print("tu as défaussé") #à changer en une méthode qui défausse
+                        done = True 
+
+                    # mettre les instructions pour faire une carte action sur le terrain
+                    else:
+                        active = False
+
+        
 
 pv = PlayerView()
 
@@ -88,9 +207,13 @@ screen = pygame.display.set_mode((960, 540))
 pygame.display.set_caption("Test")
 
 
-pv.menu("images/accueil.jpg")
-
-
+pv.menu("images/board_arena.jpg")
+pv.display_name()
+pv.pick_card()
+pv.discard_button()
+while True:
+    continue
+"""
 for i in range(9):
         for j in range(11):
             pv.field[i][j] = "images/PathBack.jpg" # Resize en 74*49
@@ -102,4 +225,4 @@ while True:
             exit()
 
     pv.field[7][7] = "images/PathBack1.jpg"
-    pv.put_card(7, 7)
+    pv.put_card(7, 7)"""
